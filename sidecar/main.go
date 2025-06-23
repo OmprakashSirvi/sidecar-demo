c429da616dd43c35232edd156cb60e0e578c3fa6
OmPrakash <OS1023901@bose.com>
2025-06-23 12:45:43 +0530

    Add Sidecar module

---
+++ sidecar/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	// Get information regarding sidecar, this will give out the routes it supports,
+	//  and some other information, This will be modified in the future.
+	router.GET("/info", handleSidecarInfo)
+}
+
+func handleSidecarInfo(c *gin.Context) {
+	c.JSON(http.StatusOK, "basic sidecar information here")
+}
