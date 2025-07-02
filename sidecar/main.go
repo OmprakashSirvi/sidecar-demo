8dc0e378b4c015d745adf088435d9abb4e288000
OmprakashSirvi <omprakashsirvi0211@gmail.com>
2025-07-02 16:52:31 +0530

    Use basic logging library

--- sidecar/main.go
+++ sidecar/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"sidecar/config"
 	"sidecar/constants"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
 	"github.com/spf13/viper"
 )
 
@@ -20,22 +22,33 @@ type Route struct {
 	Path string
 }
 
+func initLogging() {
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+}
+
+func getLogger() zerolog.Logger{
+	return zerolog.New(os.Stdout)
+}
+
 func main() {
 	router := gin.Default()
 	config.InitConfig()
+	initLogging()
+
+	logger := getLogger()
 
 	// Get information regarding sidecar, this will give out the routes it supports,
 	//  and some other information, This will be modified in the future.
 	router.GET("/info", handleSidecarInfo)
 
 	backendUrl := viper.GetString(config.GetKeyNameForEnv(constants.PROXY_BACKEND))
-	fmt.Printf("backend URL : %v\n", backendUrl)
+	logger.Debug().Str("message",fmt.Sprintf( "backend URL : %v", backendUrl))
 	ginProxy, err := NewReverseProxy(backendUrl)
 	if err != nil {
 		panic("invalid proxy backend configuration")
 	}
 
-	setProxyRoutes(router, ginProxy)
+	setProxyRoutes(router, ginProxy, logger)
 
 	// router.NoRoute(func(ctx *gin.Context) {
 	// 	// TODO: We can use the ctx to filter out any invalid or unregistered routes.
--- sidecar/proxy.go
+++ sidecar/proxy.go
@@ -9,6 +9,7 @@ import (
 	"sidecar/constants"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
 	"github.com/spf13/viper"
 )
 
@@ -29,15 +30,16 @@ func NewReverseProxy(upstream string) (*httputil.ReverseProxy, error) {
 }
 
 func UpstreamResponseModifier(r *http.Response) error {
-	fmt.Printf("got response from backend service: %v\n", r.StatusCode)
+	// Can modify the response, and see to the response and give out an error if something does not seems right
+	// Can be used in logging and tracking
 	return nil
 }
 
 // panic if something goes wrong
-func setProxyRoutes(router *gin.Engine, proxy *httputil.ReverseProxy) {
+func setProxyRoutes(router *gin.Engine, proxy *httputil.ReverseProxy, logger zerolog.Logger) {
 	// This will be a good location to get the proxy routes
 	if ok := viper.IsSet(constants.PROXY_ROUTES); !ok {
-		fmt.Println("proxy-routes is not set, hence not configuring any routes")
+		logger.Debug().Msg("proxy-routes is not set, hence not configuring any routes")
 		return
 	}
 
@@ -57,7 +59,8 @@ func setProxyRoutes(router *gin.Engine, proxy *httputil.ReverseProxy) {
 }
 
 func proxyRequestHandler(proxy *httputil.ReverseProxy, route Route) gin.HandlerFunc {
-	fmt.Printf("handling route for: %v:%v\n", route.Type, route.Path)
+	logger := getLogger()
+	logger.Debug().Msg(fmt.Sprintf("handling route for: %v:%v", route.Type, route.Path))
 	return func(ctx *gin.Context) {
 		proxy.ServeHTTP(ctx.Writer, ctx.Request)
 	}
