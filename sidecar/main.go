d4fdf219c0d4743a79036f3bf64c6615efa37be4
OmprakashSirvi <omprakashsirvi0211@gmail.com>
2025-07-02 12:37:40 +0530

    Dynamically handle routes

--- sidecar/main.go
+++ sidecar/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"net/http"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 // Backend service URL
@@ -11,9 +13,29 @@ import (
 // TODO: Make this configurable
 var PROXY_BACKEND = "http://backend:8080"
 
+type Route struct {
+	Type string
+	Path string
+}
+
+// TODO: Specify the pathname and config file name dynamically
+func initConfig() {
+	viper.SetConfigName("proxy")
+	viper.SetConfigType("yaml")
+	// TODO: Should get the config directory dynamically while initializing the service
+	// Keep this as default if config directory is not provided
+	abs, _ := filepath.Abs("/conf")
+	viper.AddConfigPath(abs)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic("not able to read proxy.yaml")
+	}
+	viper.AutomaticEnv()
+}
 
 func main() {
 	router := gin.Default()
+	initConfig()
 
 	// Get information regarding sidecar, this will give out the routes it supports,
 	//  and some other information, This will be modified in the future.
@@ -24,10 +46,12 @@ func main() {
 		panic("invalid proxy backend configuration")
 	}
 
-	router.NoRoute(func(ctx *gin.Context) {
-		// TODO: We can use the ctx to filter out any invalid or unregistered routes.
-		ginProxy.ServeHTTP(ctx.Writer, ctx.Request)
-	})
+	setProxyRoutes(router, ginProxy)
+
+	// router.NoRoute(func(ctx *gin.Context) {
+	// 	// TODO: We can use the ctx to filter out any invalid or unregistered routes.
+	// 	ginProxy.ServeHTTP(ctx.Writer, ctx.Request)
+	// })
 	router.Run()
 }
 
--- sidecar/proxy.go
+++ sidecar/proxy.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func NewReverseProxy(upstream string) (*httputil.ReverseProxy, error) {
@@ -26,4 +29,34 @@ func NewReverseProxy(upstream string) (*httputil.ReverseProxy, error) {
 func UpstreamResponseModifier(r *http.Response) error {
 	fmt.Printf("got response from backend service: %v\n", r.StatusCode)
 	return nil
+}
+
+// panic if something goes wrong
+func setProxyRoutes(router *gin.Engine, proxy *httputil.ReverseProxy) {
+	// This will be a good location to get the proxy routes
+	if ok := viper.IsSet("proxy-routes"); !ok {
+		fmt.Println("proxy-routes is not set, hence not configuring any routes")
+		return
+	}
+
+	var routes []Route
+	err := viper.UnmarshalKey("proxy-routes", &routes)
+	if err != nil {
+		panic("invalid proxy-routes configuration")
+	}
+
+	for _, route := range routes {
+		// TODO: Here need to add more switch cases
+		switch route.Type {
+		case "GET":
+			router.GET(route.Path, proxyRequestHandler(proxy, route))
+		}
+	}
+}
+
+func proxyRequestHandler(proxy *httputil.ReverseProxy, route Route) gin.HandlerFunc {
+	fmt.Printf("handling route for: %v:%v\n", route.Type, route.Path)
+	return func(ctx *gin.Context) {
+		proxy.ServeHTTP(ctx.Writer, ctx.Request)
+	}
 }
